bad552d4d1786278a0f8209e5ce5561df0774d5a
thewakar <thewakar1@gmail.com>
2023-06-25 08:35:06 +0530

    Create LoginPage.js

---
+++ LoginPage.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform login API call and handle success/failure
+    // For simplicity, let's assume the login is successful
+    const loginSuccessful = true;
+
+    if (loginSuccessful) {
+      // Redirect to the dashboard route
+      history.push('/dashboard');
+    } else {
+      // Handle login failure
+      // Display error message or take appropriate action
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
